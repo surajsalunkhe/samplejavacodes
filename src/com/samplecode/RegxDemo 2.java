a385968f1312e26bd774872a7fdae5a0cf44a25c
suraj <suraj@technosnoop.com>
2018-11-20 09:05:15 +0530

    new1

--- src/com/samplecode/RegxDemo 2.java
+++ src/com/samplecode/RegxDemo 2.java
@@ -1,7 +1,7 @@
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class RegxDemo {
+class RegxDemo {
     public static final String EXAMPLE_TEST = "This is my small example string which I'm going to use for pattern matching.";
 
     public static void main(String[] args) {
--- src/com/samplecode/TreeSetExample2.java
+++ src/com/samplecode/TreeSetExample2.java
@@ -1,6 +1,6 @@
 import java.util.TreeSet;
 
-public class TreeSetExample {
+public class TreeSetExample2{
 
     public static void main(String[] args) {
         System.out.println("Tree Set Example!\n");
